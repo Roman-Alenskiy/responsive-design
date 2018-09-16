f99dd0a89ff609e283041ae02f235fda98ccca5c
Roman Alenskiy <kuperman88@gmail.com>
2018-09-16 20:43:40 +0300

    Bug fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,18 +5,17 @@ $('document').ready(function($) {
     var main = $('main')
 
     $(window).scroll(function () {
-        if(window.matchMedia('(min-width: 768px)').matches) {
-            if ($(this).scrollTop() > 33) {
-                header.addClass("header-fixed");
-                main.css("padding-top", "4.625rem")
-                navFirst.css("display", "none");
-                headerLogo.css("padding", "12px 15px 0");
-            } else {
-                header.removeClass("header-fixed");
-                main.removeAttr("style")
-                navFirst.removeAttr("style");
-                headerLogo.removeAttr("style")
-            }
-        };
+        if ($(this).scrollTop() > 33) {
+            header.addClass("header-fixed");
+            main.css("padding-top", "4.625rem")
+            navFirst.css("display", "none");
+            headerLogo.css("padding", "12px 15px 0");
+            headerLogo.css("display", "block")
+        } else {
+            header.removeClass("header-fixed");
+            main.removeAttr("style")
+            navFirst.removeAttr("style");
+            headerLogo.removeAttr("style")
+        }
     });
 })
