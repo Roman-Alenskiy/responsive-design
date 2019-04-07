c7f7d30f0d2977d62bacf4396e760e6282a0f95b
romalenskiy <kuperman88@gmail.com>
2019-04-08 01:21:19 +0300

    getting rid of jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,21 @@
-$('document').ready(function($) {
-    var navFirst = $(".navbar-first");
-    var headerLogo = $(".header-logo");
-    var header = $('header');
-    var main = $('main')
+const navFirst = document.querySelector('.navbar-first')
+const navFirstHeight = navFirst.getBoundingClientRect().height
+const headerLogo = document.querySelector('.header-logo')
+const header = document.querySelector('header')
+const headerHeight = header.getBoundingClientRect().height
+const main = document.querySelector('main')
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 33) {
-            header.addClass("header-fixed");
-            main.css("padding-top", "4.625rem")
-            navFirst.css("display", "none");
-            headerLogo.css("padding", "12px 15px 0");
-            headerLogo.css("display", "block")
-        } else {
-            header.removeClass("header-fixed");
-            main.removeAttr("style")
-            navFirst.removeAttr("style");
-            headerLogo.removeAttr("style")
-        }
-    });
+document.addEventListener('scroll', () => {
+    if (window.pageYOffset > navFirstHeight) {
+        header.classList.add('header-fixed')
+        main.style.paddingTop = `${headerHeight}px`
+        navFirst.style.display = 'none'
+        headerLogo.style.padding = '12px 15px 0'
+        headerLogo.style.display = 'block'
+    } else {
+        header.classList.remove('header-fixed')
+        main.removeAttribute('style')
+        navFirst.removeAttribute('style')
+        headerLogo.removeAttribute('style')
+    }
 })
